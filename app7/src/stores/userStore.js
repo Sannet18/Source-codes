f2f57fe993c46e21cc012ae4cf59c48c8149d54e
Saint Appiah <saintappiah18@gmail.com>
2026-03-19 08:51:09 -0400

    Source codes

--- app7/src/stores/userStore.js
+++ app7/src/stores/userStore.js
@@ -1,25 +1,3 @@
-// import { defineStore } from 'pinia'
-// import { ref } from 'vue'
-
-// export const useUserStore = defineStore('userStore', () => {
-//   const username = ref('')
-//   const password = ref('')
-//   const loggedIn = ref(true)
-
-//   function setUser(userName, passWord) {
-//     username.value = userName
-//     password.value = passWord
-//     loggedIn.value = true
-//   }
-//   function logout() {
-//     username.value = ''
-//     password.value = ''
-//     loggedIn.value = false
-//   }
-
-//   return { username, password, setUser, logout, loggedIn }
-// })
-
 import { defineStore } from 'pinia'
 import { ref, computed } from 'vue'
 import Users from '@/modules/User'
